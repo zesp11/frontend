1350c6d10632731500c2c971c2ca3083ce37a78d
IgoRix08 <igorixmc@gmail.com>
2025-04-03 20:25:27 +0200

    Main page fix

--- src/components/generalComponents/PlainText.js
+++ src/components/generalComponents/PlainText.js
@@ -1,8 +1,11 @@
 import "./styleModules/styles.css";
-export default function PlainText({ text }) {
+export default function PlainText({ headerText,bodyText }) {
   return (
     <div className="plainTextWrapper">
-      <p className="plainText">{text}</p>
+      <div className="plainText" >
+        <h1>{headerText}</h1>
+        {bodyText}
+      </div>
     </div>
   );
 }
