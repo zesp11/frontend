9c6602bee9ec31534bbd8c6cb7e1d50788d9ffc7
IgoRix08 <igorixmc@gmail.com>
2025-04-30 12:42:29 +0200

    animated background

--- src/app/page.js
+++ src/app/page.js
@@ -4,27 +4,49 @@ import PlainText from "@/components/generalComponents/PlainText";
 export default function Home() {
   return (
     <>
-      <div className="section1" id="section1">
-      <PlainText headerText="GoTale" bodyText="Nasz projekt ułatwia integrację w zespole dzięki wspólnemu odkrywaniu opowieści, poznawaniu nowych miejsc i budowie świata na podstawie podejmowanych decyzji." /></div>
-      <div className="parallax home">
+      <div className="section" id="section1">
+        <PlainText
+          headerText="GoTale"
+          bodyText="Nasz projekt ułatwia integrację w zespole dzięki wspólnemu odkrywaniu opowieści, poznawaniu nowych miejsc i budowie świata na podstawie podejmowanych decyzji."
+        />
+        <img
+          src="/temp-logo.png"
+          alt="Ilustracja współpracy zespołowej"
+          className="animated-image"
+        />
       </div>
-      <div className="overlayblur" style={{ top: "99%" }}></div>
-      <div className="section2" id="section2">
-      <PlainText headerText=""bodyText="Projekt GoTale to innowacyjne rozwiązanie, które łączy miłośników przygód i gier miejskich w jednym interaktywnym systemie.
-       Naszym celem jest stworzenie aplikacji umożliwiającej graczom odkrywanie świata poprzez wspólne tworzenie i przeżywanie interaktywnych książek
-        przygodowych (gamebooków). Gracze mogą tworzyć zespoły, podejmować decyzje w grze, a ich działania będą wymagały fizycznego przemieszczania
-         się po mieście przy użyciu GPS." /></div>
-      <div className="parallax about">
+
+      <div className="section" id="section2">
+        <PlainText
+          headerText=""
+          bodyText="Projekt GoTale to innowacyjne rozwiązanie, które łączy miłośników przygód i gier miejskich w jednym interaktywnym systemie..."
+        />
+        <img
+          src="/logos.png" 
+          alt="Plakat gry miejskiej"
+          className="animated-image2"
+        />
       </div>
-      <div className="overlayblur" style={{ top: "199%" }}></div>
-      <div className="section3" id="section3">
-      <PlainText headerText="Możliwości projektu"bodyText="GoTale umożliwia tworzenie i edytowanie interaktywnych historii, określanie decyzji graczy, ich konsekwencji, a także lokalizacji, które gracze muszą odwiedzić w trakcie rozgrywki. Aplikacja mobilna Gamebook Explorer pozwala na odtwarzanie stworzonych historii w czasie rzeczywistym, umożliwiając zespołom dynamiczne podejmowanie decyzji i śledzenie ich postępów w grze." /></div>
-      <div className="parallax info">
+
+      <div className="section" id="section3">
+        <PlainText
+          headerText="Możliwości projektu"
+          bodyText="GoTale umożliwia tworzenie i edytowanie interaktywnych historii..."
+        />
+        <img
+          src="/main.png"
+          alt="Interaktywna książka"
+          className="animated-image3"
+        />
       </div>
-      <div className="overlayblur"style={{ top: "299%" }}></div>
-      <div className="section4" id="section4">
-      <PlainText headerText=""bodyText="Kontakt" /></div>
-      <div className="parallax contact">
+
+      <div className="section" id="section4">
+      <PlainText headerText=""bodyText="Kontakt" />
+      <img
+          src="/hero.png"
+          alt="Interaktywna książka"
+          className="animated-image4"
+        />
       </div>
     </>
   );
