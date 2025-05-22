ede452d646263b8f8a54f130f9f4932787aadd55
Cezary Arszyński <52312981+Delfin22@users.noreply.github.com>
2025-05-22 16:25:49 +0200

    Major FIX

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,12 +1,8 @@
 export const metadata = {
-  title: 'Modern OnePage',
-  description: 'Nowoczesna strona one-page w Next.js',
-}
+  title: "Modern OnePage",
+  description: "Nowoczesna strona one-page w Next.js",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="pl">
-      <body>{children}</body>
-    </html>
-  )
-} 
+  return <div>{children}</div>;
+}
